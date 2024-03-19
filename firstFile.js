5b31c89ee8d0cfd3b5be1b21d83defc1caef7480
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2024-03-18 21:27:26 -0500

    second commit"

--- firstFile.js
+++ firstFile.js
@@ -1,3 +1,6 @@
 //This is the first file created to practice different Git commands.
 // First step is to git inti. For that we write git init on the terminal.
 // To check if the git has ben initialized we write git command on the terminal and press OK. 
+// After that type git add .
+//  After adding, commit the git by typing  and entering git commit -m"whatever commit message is" 
+
