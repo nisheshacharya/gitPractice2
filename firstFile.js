1fe7a0ebee37b62ca606a71f7fa0695a20171e01
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2024-03-18 21:24:15 -0500

    first file's first commit

---
+++ firstFile.js
@@ -0,0 +1,3 @@
+//This is the first file created to practice different Git commands.
+// First step is to git inti. For that we write git init on the terminal.
+// To check if the git has ben initialized we write git command on the terminal and press OK. 
